7379410d6ed07228d2daf68453e99c77c31d1be5
Darin <dwellons@madisoncollege.edu>
2024-11-02 22:30:54 -0500

    adding a new quiz object
    using bi directional binding and bootstrap styling

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,30 +1,28 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuizService {
-
-  constructor() { }
+  constructor() {}
 
   loadQuizzes = () => {
-
     const quizzesFromWeb: any[] = [
       {
-        name: 'Quiz 1'
-        , questions: [
+        name: 'Quiz 1',
+        questions: [
           {
-            name: 'Question 1'
-          }
-          , {
-            name: 'Question 2'
-          }
-        ]
-      }
-      , {
-        name: 'Quiz 2'
-        , questions: []
-      }
+            name: 'Question 1',
+          },
+          {
+            name: 'Question 2',
+          },
+        ],
+      },
+      {
+        name: 'Quiz 2',
+        questions: [],
+      },
     ];
 
     return quizzesFromWeb;

71285970ab80179b968151af1caeeaa58dea0035
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2024-11-14 09:42:53 -0600

    Defining the shape, and shaping the data from the service
    Next, let's bind and show the quizzes...

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -6,4 +6,26 @@ import { Injectable } from '@angular/core';
 export class QuizService {
 
   constructor() { }
+
+  loadQuizzes = () => {
+    const quizzesFromWeb: any[] = [
+      {
+        name: 'Quiz 1',
+        questions: [
+          {
+            name: 'Question 1'
+          },
+          {
+            name: 'Question 2'
+          }
+        ]
+      },
+      {
+        name: 'Quiz 2',
+        questions: []
+      }
+    ];
+
+    return quizzesFromWeb;
+  };
 }

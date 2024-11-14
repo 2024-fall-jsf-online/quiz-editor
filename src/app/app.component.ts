2a39a985c44ec65b28925d457f2e06fb989b9c70
Darin <dwellons@madisoncollege.edu>
2024-11-14 17:57:54 -0600

    handle endpoint errors better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,8 @@ export class AppComponent implements OnInit {
 
   constructor(public quizSvc: QuizService) {}
 
+  errorLoadingQuizzes = false;
+
   ngOnInit() {
     const quizzes = this.quizSvc.loadQuizzes();
     console.log(quizzes);
@@ -38,7 +40,8 @@ export class AppComponent implements OnInit {
         }));
       },
       (err) => {
-        console.error(err);
+        console.error(err.error);
+        this.errorLoadingQuizzes = true;
       }
     );
 

5f6755aa850aa4f7ee0ee7a8da17c9b2adfe66b7
papadawda <dknjai1@madisoncollege.edu>
2024-11-23 21:57:39 -0600

    Using lastValueFrom rather then the deprecated toPromise

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { lastValueFrom } from 'rxjs';
 interface QuizFromWeb {
   name: string;
   questions: {
@@ -19,30 +19,32 @@ export class QuizService {
 
   loadQuizzes = () => {
 
-    const quizzesFromWeb = this.angularHttpClient.get<QuizFromWeb[]>(
+    const quizzesFromWeb = lastValueFrom(
+      this.angularHttpClient.get<QuizFromWeb[]>(
 
       "https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz"
-    );
+      )
+    );  
 
-      return quizzesFromWeb;
-  };
+      return quizzesFromWeb;    
+  }; 
   getMagicNumber = (callerWantsToSucceed: boolean): Promise<number> => {
-    return new Promise<number>(
+    return new Promise<number>( 
       (resolve, reject) => {
-
+      
         //
         // Some fancy long running code here...
         //
 
         // Ultimately resolve if successful.
         if (callerWantsToSucceed) {
-          resolve(42);
-        }
+          resolve(42); 
+        }           
         // Or reject if failure.
         else {
-          reject("Error");
-        }
-      }
+          reject("Error");                    
+        }        
+      }      
     );
   };
-}
+}    

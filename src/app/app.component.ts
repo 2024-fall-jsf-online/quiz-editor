d572b59e26b6d6d603cbf89af1752b764a9a856f
Hendrix Gullixson <hgullixson@gmail.com>
2024-11-25 17:54:32 -0600

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { QuizService, QuizFromWeb } from './quiz.service';
+import { QuizService, QuizFromWeb, ShapeForSavingEditedQuizzes, ShapeForSavingNewQuizzes } from './quiz.service';
 
 import {
   trigger,
@@ -228,5 +228,30 @@ export class AppComponent implements OnInit {
   
   detailsFromLeftAnimationDone = () => {
     this.detailsFromLeftAnimationState = "leftPosition";
+  };
+
+  saveQuizzes = async () => {
+    try {
+
+      const newQuizzes: ShapeForSavingNewQuizzes[] = [];
+      
+      const editedQuizzes: ShapeForSavingEditedQuizzes[] = this.getEditedQuizzes().map(x => ({
+        quiz: x.quizName
+        , questions: x.quizQuestions.map(y => ({
+          question: y.questionName
+        }))
+      }));
+
+      const numberOfUdpatedQuizzes = await this.quizSvc.saveQuizzes(
+        editedQuizzes, 
+        newQuizzes
+      );
+
+      console.log("numberOfUpdatedQuizzes", numberOfUdpatedQuizzes);
+  }
+    
+    catch (err) {
+      console.error(err);
+    }
   };
 }

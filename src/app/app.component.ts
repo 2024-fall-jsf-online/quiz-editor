763c6fcb3ee6e79358f6148f819437754b0cecf4
Darin <dwellons@madisoncollege.edu>
2024-11-29 16:17:35 -0600

    changed get new quizzes to get added quizzes
    is now working, posted to slack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -252,7 +252,7 @@ export class AppComponent implements OnInit {
 
   saveQuizzes = async () => {
     try {
-      const newQuizzes: ShapeForSavingNewQuizzes[] = this.getNewQuizzes().map(
+      const newQuizzes: ShapeForSavingNewQuizzes[] = this.getAddedQuizzes().map(
         (x) => ({
           quizName: x.quizName,
           quizQuestions: x.quizQuestions.map((y) => y.questionName),

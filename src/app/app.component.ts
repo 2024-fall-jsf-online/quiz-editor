cf5926617bd8b48f3ac8fd11b870aa41badda4cb
papadawda <dknjai1@madisoncollege.edu>
2024-12-09 15:36:39 -0600

    Got edited  quiz badge working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService, QuizFromWeb } from './quiz.service';
+import { generate } from 'rxjs';
 
 interface QuizDisplay {
   quizName: string;
   quizQuestions: QuestionDisplay[];
   markedForDelete: boolean;
   newlyAddedQuiz: boolean;
+  naiveQuizChecksum: string;
 }
 
 interface QuestionDisplay {
@@ -28,6 +30,11 @@ export class AppComponent implements OnInit {
   loading = true;
   errorLoadingQuizzes = false;
 
+  generateNaiveQuizChecksum = (quiz: QuizFromWeb) => {
+    return quiz.name + quiz.questions.map(x => '~' + x.name).join('');
+
+  };
+
   loadQuizzesFromCloud = async () => {
 
     try {
@@ -41,6 +48,7 @@ export class AppComponent implements OnInit {
         }))
         , markedForDelete: false
         , newlyAddedQuiz: false
+        , naiveQuizChecksum: this.generateNaiveQuizChecksum(x)
       }));      
 
       this.loading = false;
@@ -71,6 +79,7 @@ export class AppComponent implements OnInit {
       , quizQuestions: []
       , markedForDelete: false
       , newlyAddedQuiz: true
+      , naiveQuizChecksum: ""
     };
 
     this.quizzes = [
@@ -173,5 +182,17 @@ export class AppComponent implements OnInit {
 
     get addedQuizCount() {
       return this.getAddedQuizzes().length;
+    }
+
+    getEditedQuizzes = () => {
+      return this.quizzes.filter(x => 
+        x.quizName + x.quizQuestions.map(y => '~' + y.questionName).join('') !== x.naiveQuizChecksum
+        && !x.newlyAddedQuiz 
+        && !x.markedForDelete
+      );
+    };
+
+    get editedQuizCount() {
+      return this.getEditedQuizzes().length;
     }
 }

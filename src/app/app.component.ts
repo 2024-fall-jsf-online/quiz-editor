b68dd074535a6677874b2e7ea5aeb0cf4bf12ab1
timmy1233 <kazumaotosensei@gmail.com>
2024-12-12 10:05:30 -0600

    Selecting a quiz and keeping track of it
    yep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,4 +38,9 @@ export class AppComponent implements OnInit {
   }
 
   quizzes: QuizDisplay[] = [];
+  selectedQuiz: QuizDisplay | undefined = undefined;
+  selectQuiz = (q: QuizDisplay) => {
+    this.selectedQuiz = q;
+    console.log(this.selectedQuiz);
+  };
 }
